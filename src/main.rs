2f7690ff6f9803d8a2aefc118beae7e09a936eda
Ueno <tomonori_ueno@sezax.co.jp>
2022-09-22 13:21:05 +0700

    initialize Rocket project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "This is my first Rocket Demo app"
+}
+
+#[get("/about")]
+fn about() -> &'static str {
+    "This is the about page of my Rocket Demo app"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index, about]).launch();
 }
