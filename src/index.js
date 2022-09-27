a1c894cd5d4fe813f6eec8b4cc44861bcfd473b9
Ueno <tomonori_ueno@sezax.co.jp>
2022-09-27 10:20:13 +0700

    Update index.js main.rs

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const wasm = import("./pkg/rocket_demo_app_bg");
+
+wasm.then(m => {
+  const App = () => {
+
+    return ( 
+      <>
+        <div>
+          <h1>Hi there</h1>
+          <button onClick={m.big_computation}>My birthday is...</button>
+        </div>
+      </>
+    );
+  };
+
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,10 @@
 #[macro_use] 
 extern crate rocket;
 
-use std::io;
-use std::path::{Path, PathBuf};
-
-use rocket::response::NamedFile;
-
 #[get("/")]
-fn index() -> io::Result<NamedFile> {
-    NamedFile::open("src/index.jsx")
+fn index() -> &'static str {
+    "This is my first Rocket Demo app"
 }
-
-// #[get("/")]
-// fn index() -> &'static str {
-//     "This is my first Rocket Demo app"
-// }
-
-// #[get("/about")]
-// fn about() -> &'static str {
-//     "This is the about page of my Rocket Demo app"
-// }
 
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
